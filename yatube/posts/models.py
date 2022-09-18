a9e2e6b9c90ece0873028c8df2477421b17f606b
Kovaleva Anastasia <anastasia-yurjevna@yandex.ru>
2022-09-19 02:45:00 +0700

    yatube3

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 
 User = get_user_model()
 
+
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(null=False, unique=True)
@@ -15,8 +16,8 @@ class Group(models.Model):
 
     def get_absolute_url(self):
         return reverse("group_posts", kwargs={"slug": self.slug})
-        
-    
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -24,8 +25,8 @@ class Post(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='posts'
-    ) 
-    group = models.ForeignKey(Group, 
+    )
+    group = models.ForeignKey(Group,
                               blank=True,
                               null=True,
                               on_delete=models.CASCADE,
